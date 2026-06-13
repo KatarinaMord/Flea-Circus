73476377aae508fd3b7a961a98cf7ba8dc18a286
KatarinaMord <itscatmora@gmail.com>
2026-06-13 02:42:29 +0200

    Add files via upload

---
+++ mover.js
@@ -0,0 +1,152 @@
+class Mover {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = p5.Vector.random2D();
+    this.velocity.setMag(2);
+    this.acceleration = createVector(0, 1);
+    this.t = random(0, 1000);
+    colorMode(HSB, 360, 100, 100, 100);
+    this.prevPosition = this.position.copy();
+    //this.life = 255;
+  }
+
+  display() {
+    noFill();
+    strokeWeight(5);
+    line(this.prevPosition.x,this.prevPosition.y,this.position.x,this.position.y);
+  }
+
+  update() {
+    this.prevPosition = this.position.copy();
+
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(5);
+    this.position.add(this.velocity);
+
+    this.acceleration.mult(0);
+    this.velocity.rotate(random(-0.5, 0.5));
+    //this.life -= 1;
+  }
+  
+  wheelDistance(){
+    let d = dist(width / 2, height / 2, this.position.x, this.position.y);
+    return d;
+  }
+  
+
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
+
+  spawn(angleOffset = 0) {
+    let child = new Mover(this.position.x, this.position.y, this.mass);
+    child.velocity = this.velocity.copy();
+    child.velocity.rotate(random(-0.1, 0.1) + angleOffset);
+    child.velocity.mult(0.9);
+    return child;
+  }
+
+  wrapEdges() {
+    if (this.position.x > width) this.position.x = 0;
+    if (this.position.x < 0) this.position.x = width;
+    if (this.position.y > height) this.position.y = 0;
+    if (this.position.y < 0) this.position.y = height;
+
+    this.prevPosition = this.position.copy();
+  }
+
+  seek(target) {
+    let desired = p5.Vector.sub(target, this.position);
+    desired.setMag(4);
+
+    let steer = p5.Vector.sub(desired, this.velocity);
+    steer.limit(0.15);
+    return steer;
+  }
+
+  separateForce(others) {
+    let perceptionRadius = 28;
+    let steering = createVector(0, 0);
+    let total = 0;
+
+    for (let other of others) {
+      let d = p5.Vector.dist(this.position, other.position);
+
+      if (other !== this && d < perceptionRadius) {
+        let diff = p5.Vector.sub(this.position, other.position);
+        diff.div(d * d);
+        steering.add(diff);
+        total++;
+      }
+    }
+
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(4);
+      steering.sub(this.velocity);
+      steering.limit(0.22);
+    }
+
+    return steering;
+  }
+
+  alignForce(others) {
+    let perceptionRadius = 55;
+    let steering = createVector(0, 0);
+    let total = 0;
+
+    for (let other of others) {
+      let d = p5.Vector.dist(this.position, other.position);
+
+      if (other !== this && d < perceptionRadius) {
+        steering.add(other.velocity);
+        total++;
+      }
+    }
+
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(4);
+      steering.sub(this.velocity);
+      steering.limit(0.08);
+    }
+
+    return steering;
+  }
+
+  cohesionForce(others) {
+    let perceptionRadius = 55;
+    let center = createVector(0, 0);
+    let total = 0;
+
+    for (let other of others) {
+      let d = p5.Vector.dist(this.position, other.position);
+
+      if (other !== this && d < perceptionRadius) {
+        center.add(other.position);
+        total++;
+      }
+    }
+
+    if (total > 0) {
+      center.div(total);
+      return this.seek(center);
+    }
+
+    return createVector(0, 0);
+  }
+
+  flock(others) {
+    let separation = this.separateForce(others);
+    let alignment = this.alignForce(others);
+    let cohesion = this.cohesionForce(others);
+
+    separation.mult(1.7);
+    alignment.mult(1.0);
+    cohesion.mult(0.9);
+
+    this.applyForce(separation);
+    this.applyForce(alignment);
+    this.applyForce(cohesion);
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,106 @@
+let movers = [];
+let cannon;
+let wheel;
+let ground;
+let wormhole;
+let song;
+let squeak;
+let volumeSlider;
+let angle = 0;
+let rotation = 0;
+
+function preload() {
+  wheel = loadImage("wheel.png");
+  ground = loadImage("background.png")
+  wormhole = loadImage("wormhole.png")
+  song = loadSound("song.mp3");
+  squeak = loadSound("squeak.mp3");
+}
+
+function setup() {
+  song.loop();
+  createCanvas(windowWidth, windowHeight);
+  noFill();
+  for (let i = 0; i < 5; i++) {
+    let m = new Mover(random(width), random(height));
+    movers.push(m);
+  }
+
+  imageMode(CENTER);
+  angleMode(DEGREES);
+
+  volumeSlider = createSlider(0, 1, 0.5, 0.01);
+  volumeSlider.position(115, 10);
+  volumeSlider.size(80);
+}
+
+function draw() {
+  image(ground, width/2, height/2);
+  fill(255, 0, 100, 8);
+  noStroke();
+  rect(0,0,width,height);
+  song.setVolume(volumeSlider.value());
+
+  fill("#A03434");
+  stroke("#A03434");
+  textSize(16);
+  strokeWeight(0.5);
+  text("Song volume:", 10, 24);
+
+  push();
+  translate(width / 2, height / 2);
+  rotate(angle);
+  image(wheel, 0, 0, height / 2, height / 2);
+  pop();
+
+  push();
+  translate(width/4, height/4);
+  rotate(rotation);
+  image(wormhole, 0,0, height/8, height/8);
+  pop();
+
+
+  push();
+  translate(width*0.73, height*0.75,);
+  rotate(rotation);
+  image(wormhole, 0, 0, height/8, height/8);
+  pop();
+
+  spinSpeed = 1;
+  stroke(0);
+  for (let mover of movers) {
+    //mover.flock(movers);   
+    mover.update();
+    
+    if(mover.wheelDistance() < (height*0.2) ){
+      spinSpeed = 5;
+      mover.velocity.mult(5);
+    }
+
+    let d1 = dist(width/4, height/4, mover.position.x, mover.position.y);
+    let d2 = dist(width*0.73, height*0.75, mover.position.x, mover.position.y);
+
+    if(d1 < height/16){
+      mover.position.x = width*0.78;
+      mover.position.y = height*0.8;
+      squeak.play();
+    }
+
+    if(d2 < height/16){
+      mover.position.x = width/4.5;
+      mover.position.y = height/4.5;
+      squeak.play();
+    }
+    
+    mover.wrapEdges();
+    mover.display();
+  }
+  
+  rotation = rotation - 2;
+  angle = angle + spinSpeed;
+}
+
+function mousePressed() {
+  let mover = new Mover(mouseX, mouseY, 20);
+  movers.push(mover);
+}
